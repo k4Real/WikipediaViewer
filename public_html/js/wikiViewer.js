f3e092a5b0748c9947c1c99e347264983a64263d
Karels Jacques <karelsjacques@gmail.com>
2017-08-22 20:44:31 +0200

    Ajax function
    a lot of stuff

--- public_html/js/wikiViewer.js
+++ public_html/js/wikiViewer.js
@@ -2,8 +2,61 @@
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
- * 
+ * Switch between different wikipedia languages
  * bind enter to pressing key
  */
+ var searchUrl="https://en.wikipedia.org/w/api.php?" ;
+ var searchText="";
 
 
+$(document).ready(function() {
+    
+
+    $("#searchField").keypress(function(event){
+    
+    if (event.key==='Enter')
+    {
+         searchText = this.value;
+         
+      searchUrl ="https://en.wikipedia.org/w/api.php?action=opensearch&format=json&origin=*&uselang=user&search="+searchText +"&limit=20&profile=fuzzy";
+       $.ajax ({ 
+           
+         url : searchUrl ,
+         
+         success: function(response){
+             
+             for (i=0; i<20;i++)
+             {
+               
+                 var appendString ="<div class='searchResult'> <p> <a class='title' href="+response[3][i] +">";
+                 appendString += response[1][i]+"</a>" ;
+                 appendString += "<br>" + response[2][i] ;
+                appendString +="</p></div>" ; 
+                 
+                $("#content").append(appendString); 
+                 
+             }
+             
+             
+             
+         },
+         
+         error :function (){}
+         
+         
+       
+       
+        
+    });
+    
+    
+        }  
+    
+    
+    
+})
+    
+    
+    
+ 
+}) ;
